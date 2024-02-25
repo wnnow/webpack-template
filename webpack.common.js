62d130957b77ae580c4d3ee99190fca1cec04ded
wnnow <117201813+wnnow@users.noreply.github.com>
2024-02-25 16:42:50 +0700

    Add webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -2,18 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      title: 'Development',
+      title: 'Production',
     }),
   ],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './src',
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
